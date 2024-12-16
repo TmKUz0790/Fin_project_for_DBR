9c0c70abdcfc4e4c4b55f72cccd991193a34e1e4
TmKUz0790 <104360495+TmKUz0790@users.noreply.github.com>
2024-12-16 11:58:40 +0500

    n1

--- Fin_v2_by/settings.py
+++ Fin_v2_by/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'Fin_v2_by.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mysql.connector.django',  # Using the MySQL connector for Django
+        'NAME': 'railway',                   # Database name
+        'USER': 'root',                      # Database user
+        'PASSWORD': 'cvokSRmvtuekukuhkbssEWUPJubkDqQA',  # Database password
+        'HOST': 'junction.proxy.rlwy.net',   # New proxy host address
+        'PORT': '20556',                     # New proxy port number
     }
 }
 
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Fin_v2_by.wsgi:application --bind 0.0.0.0:$PORT --timeout 120
